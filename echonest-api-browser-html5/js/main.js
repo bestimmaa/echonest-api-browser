de83738f2a43ec7fdaa6b0b0f3531c0a79182343
Christoph Halang <christoph.halang@gmail.com>
2014-12-04 15:30:34 +0100

    added JSONP query to echo nest

--- echonest-api-browser-html5/js/main.js
+++ echonest-api-browser-html5/js/main.js
@@ -1,15 +1,19 @@
 $(function () {
     $("#search_song_button").click(function () {
-        alert('clicked!');
+        //alert('clicked!');
+        getExampleJSONP();
     });
 });
 
-function getExample() {
-    jQuery.getJSON("http://developer.echonest.com/api/v4/song/search?api_key=FILDTEOIK2HBORODV&format=json&results=1&artist=radiohead&title=karma%20police",
-        function () {
-            console.log("success")
+function getExampleJSONP() {
+    jQuery.ajax({
+        dataType: "jsonp",
+        url: "http://developer.echonest.com/api/v4/song/search?api_key=QTJ926WQJ92B089WB&format=jsonp",
+        data: {"results": 1, "artist": "Radiohead", "title": "Karma Police"},
+        success: function (response) {
+            console.log(response);
         }
-    )
+    });
 }
 
 
