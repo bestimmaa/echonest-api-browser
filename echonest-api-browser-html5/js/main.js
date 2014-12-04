67d6304cf3869726e9438bdbfded399762723de2
Christoph Halang <christoph.halang@gmail.com>
2014-12-04 14:47:21 +0100

    added search button

--- echonest-api-browser-html5/js/main.js
+++ echonest-api-browser-html5/js/main.js
@@ -1,1 +1,15 @@
+$(function () {
+    $("#search_song_button").click(function () {
+        alert('clicked!');
+    });
+});
+
+function getExample() {
+    jQuery.getJSON("http://developer.echonest.com/api/v4/song/search?api_key=FILDTEOIK2HBORODV&format=json&results=1&artist=radiohead&title=karma%20police",
+        function () {
+            console.log("success")
+        }
+    )
+}
+
 
