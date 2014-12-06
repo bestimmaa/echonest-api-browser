b1ded1f065b95fa978373cb1bbdb78b65faf3a1d
Christoph Halang <christoph.halang@gmail.com>
2014-12-06 17:16:58 +0100

    moving lines, doing stuff, reformatting

--- echonest-api-browser-html5/js/main.js
+++ echonest-api-browser-html5/js/main.js
@@ -1,24 +1,28 @@
 // Read api key from echonest_api_key file
 var api_key;
+var musical_key;
+var mode;
+var tempo;
+var energy;
+var danceability;
+var acousticness;
+var speechiness;
+
 
 
 $(function () {
-    // read api key from file
+    // read api key from file and store to global variable
     apiKey();
 
     $("#search_song_button").click(function () {
-        //alert('clicked!');
         getExampleJSONP();
     });
-    $("#slider_tempo").slider();
 
+    $("#slider_tempo").slider();
     $("#slider_energy").slider();
     $("#slider_danceability").slider();
     $("#slider_acousticness").slider();
     $("#slider_speechiness").slider();
-
-
-
 });
 
 
@@ -26,20 +30,20 @@ function getExampleJSONP() {
     jQuery.ajax({
         dataType: "jsonp",
         url: "http://developer.echonest.com/api/v4/song/search?format=jsonp",
-        data: {"results": 1, "artist": "Radiohead", "title": "Karma Police", "api_key":api_key},
+        data: {"results": 1, "artist": "Radiohead", "title": "Karma Police", "api_key": api_key},
         success: function (response) {
             console.log(response);
 
             $(".CSSTableGenerator").remove();
 
-            var header = ["artist_id","id","artist_name","title"];
+            var header = ["artist_id", "id", "artist_name", "title"];
 
             var songs = response["response"]["songs"];
             var tableRows = new Array();
             tableRows.push(header);
-            for (i = 0; i < songs.length; ++i){
+            for (i = 0; i < songs.length; ++i) {
                 var row = new Array();
-                for (var s in songs[i]){
+                for (var s in songs[i]) {
                     row.push(String(songs[i][s]));
                 }
                 tableRows.push(row);
@@ -51,14 +55,21 @@ function getExampleJSONP() {
     });
 }
 
+function apiKey() {
+    jQuery.getJSON('echonest_api_key', function (data) {
+        console.log("Found API Key: " + data.api_key);
+        api_key = (String(data.api_key));
+    });
+}
+
 // the following functions are taken from http://www.htmlgoodies.com/beyond/css/working_w_tables_using_jquery.html
 
 function makeTable(container, data) {
     var table = $("<table/>").addClass('CSSTableGenerator');
-    $.each(data, function(rowIndex, r) {
+    $.each(data, function (rowIndex, r) {
         var row = $("<tr/>");
-        $.each(r, function(colIndex, c) {
-            row.append($("<t"+(rowIndex == 0 ?  "h" : "d")+"/>").text(c));
+        $.each(r, function (colIndex, c) {
+            row.append($("<t" + (rowIndex == 0 ? "h" : "d") + "/>").text(c));
         });
         table.append(row);
     });
@@ -67,18 +78,12 @@ function makeTable(container, data) {
 
 function appendTableColumn(table, rowData) {
     var lastRow = $('<tr/>').appendTo(table.find('tbody:last'));
-    $.each(rowData, function(colIndex, c) {
+    $.each(rowData, function (colIndex, c) {
         lastRow.append($('<td/>').text(c));
     });
 
     return lastRow;
 }
 
-function apiKey(){
-    jQuery.getJSON('echonest_api_key',function(data){
-        // data is an array of objects
-        console.log("Found API Key: " + data.api_key);
-        api_key = (String(data.api_key));
-    });
-}
+
 
